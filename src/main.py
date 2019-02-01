6eeb107a1df8335afabb7116ad36664c782fffdb
rvsandeep <rvsandeep@outlook.com>
2019-02-01 02:27:11 -0800

    entry point for spark jobs

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
 import pyspark
+import sys
+import os
+
+if os.path.exists('jobs.zip'):
+    sys.path.insert(0, 'jobs.zip')
+else:
+    sys.path.insert(0, './jobs')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--job', type=str, required=True)
+parser.add_argument('--job-args', nargs='*')
+args = parser.parse_args()
+
+sc = pyspark.SparkContext(appName=args.job_name)
+job_module = importlib.import_module('jobs.%s' % args.job)
+job_module.analyze(sc, job_args)

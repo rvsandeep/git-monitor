b1a32e577d84bfb94a22184382cb210d59a59a8f
rvsandeep <rvsandeep@outlook.com>
2019-02-01 03:08:26 -0800

    add create project nodes jobs

---
+++ src/jobs/create_project_nodes.py
@@ -0,0 +1,18 @@
+from py2neo import Graph
+import credentials
+
+def analyze(sc):
+    gg = Graph( bolt=True,
+                host=credentials.NEO4J_HOST, secure=True,
+                user=credentials.NEO4J_USERNAME, password=credentials.NEO4J_PASSWORD)
+
+    hadoop_conf=sc._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    hadoop_conf.set("fs.s3a.awsAccessKeyId", os.environ['AWS_ACCESS_KEY_ID'])
+    hadoop_conf.set("fs.s3a.awsSecretAccessKey", os.environ['AWS_SECRET_ACCESS_KEY'])
+
+    projects = spark.read.format("csv").option("header", "true").load("s3a://rvsandeep-bucket/projects.csv")
+
+    projects_rdd_tf = projects.rdd.map(lambda x : util.populate_project(models.Project(), x))
+
+    projects_rdd_tf.take(5)

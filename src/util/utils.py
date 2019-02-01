9cd11f9b28bb1c8a2325376e87833bfceb7c676e
rvsandeep <rvsandeep@outlook.com>
2019-01-31 19:24:40 -0800

    adding objects

---
+++ src/util/utils.py
@@ -0,0 +1,12 @@
+def populate_project(project_node, project):
+    project_node.project_id = project['ID']
+    project_node.name = project['Name']
+    project_node.created_timestamp = project['Created Timestamp']
+    project_node.updated_timestamp = project['Updated Timestamp']
+    project_node.description = project['Description']
+    project_node.homepage = project['Homepage URL']
+    project_node.source_rank = project['SourceRank']
+    project_node.latest_release_publish_timestamp = project['Latest Release Publish Timestamp']
+    project_node.latest_release_number = project['Latest Release Number']
+    project_node.last_synced_timestamp = project['Last synced Timestamp']
+    return project_node

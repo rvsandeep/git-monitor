457e5539f4b00969db83ddea9f6b219ca9502fec
rvsandeep <rvsandeep@outlook.com>
2019-02-01 16:17:58 -0800

    not sure what i'm fixing at this point

--- src/jobs/create_project_nodes.py
+++ src/jobs/create_project_nodes.py
@@ -7,10 +7,10 @@ def analyze(sc, job_args=None):
                 host=credentials.NEO4J_HOST, secure=True,
                 user=credentials.NEO4J_USERNAME, password=credentials.NEO4J_PASSWORD)
 
-    hadoop_conf=sc._jsc.hadoopConfiguration()
-    hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    hadoop_conf.set("fs.s3a.awsAccessKeyId", credentials.AWS_ACCESS_KEY_ID)
-    hadoop_conf.set("fs.s3a.awsSecretAccessKey", credentials.AWS_SECRET_ACCESS_KEY)
+    #hadoop_conf=sc._jsc.hadoopConfiguration()
+    #hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    #hadoop_conf.set("fs.s3a.awsAccessKeyId", credentials.AWS_ACCESS_KEY_ID)
+    #hadoop_conf.set("fs.s3a.awsSecretAccessKey", credentials.AWS_SECRET_ACCESS_KEY)
 
     # Creating SparkSession, Spark Context and SQL Context Objects
     spark = SparkSession.builder \
@@ -20,7 +20,7 @@ def analyze(sc, job_args=None):
     	    .config("spark.sql.join.preferSortMergeJoin", "false") \
     	    .getOrCreate()
 
-    projects = spark.read.format("csv").option("header", "false").load("s3a://rvsandeep-bucket/projects.csv")
+    projects = spark.read.csv("s3n://rvsandeep-bucket/projects.csv", header=True, inferSchema=True, multiLine=True, escape='"'')
 
     projects_rdd_tf = projects.rdd.map(lambda x : util.populate_project(models.Project(), x))
 

cff7a75f7d4e7847e8d02652813320c4e0ea51bf
rvsandeep <rvsandeep@outlook.com>
2019-02-03 19:45:13 -0800

    setting up rync project to aws, instead of committing files

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,1 +1,7 @@
-rsync -avze /Users/rvsandeep/git-monitor/scripts/sync.sh ssh ubuntu@ec2-52-34-246-118.us-west-2.compute.amazonaws.com:/home/ubuntu/temp/git-monitor/scripts/
+rsync -avL --progress -e "ssh -i /Users/rvsandeep/.ssh/rvsandeep-IAM-keypair.pem" \
+      --exclude='.git/' \
+      --exclude='dist/' \
+      --exclude='env/' \
+      --exclude='src/libs/' \
+      --exclude='*/__pycache__/' \
+      /Users/rvsandeep/git-monitor/ ubuntu@ec2-52-34-246-118.us-west-2.compute.amazonaws.com:/home/ubuntu/git-monitor/

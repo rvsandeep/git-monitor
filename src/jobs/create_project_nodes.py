5988a2207d2e0146896fc449364cbd98414eb137
rvsandeep <rvsandeep@outlook.com>
2019-02-04 23:51:33 -0800

    updating project data model

--- src/jobs/create_project_nodes.py
+++ src/jobs/create_project_nodes.py
@@ -1,25 +1,16 @@
-from py2neo import Graph
 import credentials
 from pyspark.sql import SparkSession
 from utils import util
+from models.project import Project
 
 def analyze(sc, job_args=None):
-    gg = Graph( bolt=True,
-                host=credentials.NEO4J_HOST, secure=True,
-                user=credentials.NEO4J_USERNAME, password=credentials.NEO4J_PASSWORD)
+    # Creating SparkSession
+    spark = SparkSession.builder.appName("git-monitor").getOrCreate()
 
-    #hadoop_conf=sc._jsc.hadoopConfiguration()
-    #hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    #hadoop_conf.set("fs.s3a.awsAccessKeyId", credentials.AWS_ACCESS_KEY_ID)
-    #hadoop_conf.set("fs.s3a.awsSecretAccessKey", credentials.AWS_SECRET_ACCESS_KEY)
 
-    # Creating SparkSession, Spark Context and SQL Context Objects
-    spark = SparkSession.builder \
-                .appName("S3 READ TEST") \
-                .config("spark.executor.cores", "6") \
-                .config("spark.executor.memory", "6gb") \
-    	    .config("spark.sql.join.preferSortMergeJoin", "false") \
-    	    .getOrCreate()
-
-    projects = spark.read.format("csv").option("header", "false").load("s3a://rvsandeep-bucket/projects.csv")
-    print(projects.collect())
+    projects = spark.read.format("csv") \
+                .option("header", "true") \
+                .load("s3a://rvsandeep-bucket/projects.csv")
+    prj_nodes_rdd = projects.rdd.map(lambda x : Project(x))
+    gc = util.GraphConnector()
+    gc.create(prj_nodes_rdd.collect())
--- src/utils/util.py
+++ src/utils/util.py
@@ -1,15 +1,16 @@
 import models
+from py2neo import Graph
+import credentials
 
-def populate_project(project):
-    project_node = models.Project()
-    project_node.project_id = project['ID']
-    project_node.name = project['Name']
-    project_node.created_timestamp = project['Created Timestamp']
-    project_node.updated_timestamp = project['Updated Timestamp']
-    project_node.description = project['Description']
-    project_node.homepage = project['Homepage URL']
-    project_node.source_rank = project['SourceRank']
-    project_node.latest_release_publish_timestamp = project['Latest Release Publish Timestamp']
-    project_node.latest_release_number = project['Latest Release Number']
-    project_node.last_synced_timestamp = project['Last synced Timestamp']
-    return project_node
+class GraphConnector():
+
+    def __init__(self):
+        self.connector = Graph( bolt=True,
+                    host=credentials.NEO4J_HOST, secure=True,
+                    user=credentials.NEO4J_USERNAME, password=credentials.NEO4J_PASSWORD)
+
+    def create(self, nodes):
+        tx = self.connector.begin()
+        for each in nodes:
+            tx.create(each)
+        tx.commit()
